1c495f3dc64947c680a25aa90e44007e77a970c9
BehemothDi <dmitriiprozherin@mail.ru>
2023-01-12 11:23:59 +0300

    Initial commit

---
+++ src/Svenn.java
@@ -0,0 +1,17 @@
+public class Svenn {
+
+    static class Function {
+
+        public double value(double x){
+            return Math.pow(x, 2);
+        }
+
+    }
+
+    public static double[] find(double a, double b, Function function) {
+        
+    }
+
+
+
+}
